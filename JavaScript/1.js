0527c97fa34de37d6cba08c7838d83000c241071
Sachi-CloudAnalogy <sachi.gaur@cloudanalogy.com>
2024-02-13 19:01:05 +0530

    Day-1

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -1,2 +1,7 @@
 console.log("First Code of JS");
 console.log("Helllllooooo !!");
+
+// one line comments
+
+/* multi line
+comments*/
---
+++ JavaScript/2.js
@@ -0,0 +1,14 @@
+//Variables
+
+let fullName = "priya sharma";
+let age = 25;
+let price = 99.99;
+
+age = 27;
+
+//Constants
+const year = 2024;
+
+let developer;
+developer = fullName;
+alert(developer);
